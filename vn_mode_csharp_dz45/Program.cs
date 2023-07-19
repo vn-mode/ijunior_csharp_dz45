8c21784c4a2022772cbb6045f965c12fdd960755
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-19 18:59:01 +0300

    ijunior_csharp_dz45

--- vn_mode_csharp_dz45/Program.cs
+++ vn_mode_csharp_dz45/Program.cs
@@ -3,35 +3,15 @@ using System.Collections.Generic;
 
 public static class Program
 {
-    private static string _fighter1Name = "Warrior1";
-    private static int _fighter1Health = 100;
-    private static int _fighter1Damage = 20;
-
-    private static string _fighter2Name = "Warrior2";
-    private static int _fighter2Health = 100;
-    private static int _fighter2Damage = 20;
-
-    private static string _fighter3Name = "Rogue";
-    private static int _fighter3Health = 90;
-    private static int _fighter3Damage = 15;
-
-    private static string _fighter4Name = "Priest";
-    private static int _fighter4Health = 80;
-    private static int _fighter4Damage = 10;
-
-    private static string _fighter5Name = "Mage";
-    private static int _fighter5Health = 70;
-    private static int _fighter5Damage = 15;
-    private static int _fighter5Mana = 100;
-
     public static void Main()
     {
-        List<Fighter> fighters = new List<Fighter> {
-            new Warrior(_fighter1Name, _fighter1Health, _fighter1Damage),
-            new Warrior(_fighter2Name, _fighter2Health, _fighter2Damage),
-            new Rogue(_fighter3Name, _fighter3Health, _fighter3Damage),
-            new Priest(_fighter4Name, _fighter4Health, _fighter4Damage),
-            new Mage(_fighter5Name, _fighter5Health, _fighter5Damage, _fighter5Mana)
+        List<Fighter> fighters = new List<Fighter>
+        {
+            new Warrior("Warrior1", 100, 20),
+            new Warrior("Warrior2", 100, 20),
+            new Rogue("Rogue", 90, 15),
+            new Priest("Priest", 80, 10),
+            new Mage("Mage", 70, 15, 100)
         };
 
         Console.WriteLine("Выберите двух бойцов для битвы:");
@@ -50,45 +30,44 @@ public static class Program
         while (!fighter1.IsDead() && !fighter2.IsDead())
         {
             fighter1.Attack(fighter2);
-            if (fighter2.IsDead()) break;
+
+            if (fighter2.IsDead()) continue;
             fighter2.Attack(fighter1);
         }
 
-        if (fighter1.IsDead())
-        {
-            Console.WriteLine(Messages.WinMessage, fighter2.Name, fighter1.Name);
-        }
-        else
-        {
-            Console.WriteLine(Messages.WinMessage, fighter1.Name, fighter2.Name);
-        }
+        Console.WriteLine(fighter1.IsDead()
+            ? string.Format(Messages.WinMessage, fighter2.Name, fighter1.Name)
+            : string.Format(Messages.WinMessage, fighter1.Name, fighter2.Name));
     }
 
     private static int GetUserChoice(int maxChoice)
     {
-        int choice;
+        bool validInput = false;
 
-        while (true)
+        while (!validInput)
         {
+            int choice = 0;
             Console.Write("Введите число: ");
-            if (int.TryParse(Console.ReadLine(), out choice) && choice >= 1 && choice <= maxChoice)
-            {
-                break;
-            }
-            else
+            validInput = int.TryParse(Console.ReadLine(), out choice) && choice >= 1 && choice <= maxChoice;
+
+            if (!validInput)
             {
                 Console.WriteLine($"Неверный ввод. Введите число от 1 до {maxChoice}.");
             }
+            else
+            {
+                return choice;
+            }
         }
-        return choice;
+        return 0; // This line will never be reached but is necessary for compilation
     }
 }
 
 public abstract class Fighter
 {
-    public string Name { get; set; }
-    public int Health { get; set; }
-    public int Damage { get; set; }
+    public string Name { get; private set; }
+    public int Health { get; protected set; }
+    public int Damage { get; private set; }
 
     protected Fighter(string name, int health, int damage)
     {
@@ -104,12 +83,14 @@ public abstract class Fighter
     public void TakeDamage(int damage)
     {
         Health -= damage;
+
         if (Health < 0) Health = 0;
     }
 
     public void Attack(Fighter enemy)
     {
         SpecialAttack(enemy);
+
         if (enemy.IsDead())
         {
             Console.WriteLine(Messages.DeathMessage, enemy.Name);
@@ -119,6 +100,8 @@ public abstract class Fighter
 
 public class Warrior : Fighter
 {
+    private const int AttackMultiplier = 2;
+    private const int SpecialAttackFrequency = 3;
     private int _attackCounter = 0;
 
     public Warrior(string name, int health, int damage) : base(name, health, damage) { }
@@ -126,7 +109,7 @@ public class Warrior : Fighter
     public override void SpecialAttack(Fighter enemy)
     {
         _attackCounter++;
-        int attackDamage = _attackCounter % 3 == 0 ? Damage * 2 : Damage;
+        int attackDamage = _attackCounter % SpecialAttackFrequency == 0 ? Damage * AttackMultiplier : Damage;
         enemy.TakeDamage(attackDamage);
         Console.WriteLine(Messages.DamageMessage, Name, attackDamage, enemy.Name, enemy.Health);
     }
@@ -135,12 +118,13 @@ public class Warrior : Fighter
 public class Rogue : Fighter
 {
     private static Random _rnd = new Random();
+    private const int DodgeChance = 30;
 
     public Rogue(string name, int health, int damage) : base(name, health, damage) { }
 
     public override void SpecialAttack(Fighter enemy)
     {
-        if (_rnd.Next(100) < 30)
+        if (_rnd.Next(100) < DodgeChance)
         {
             Console.WriteLine(Messages.DodgeMessage, Name);
         }
@@ -154,19 +138,23 @@ public class Rogue : Fighter
 
 public class Priest : Fighter
 {
+    private const int HealMultiplier = 2;
+
     public Priest(string name, int health, int damage) : base(name, health, damage) { }
 
     public override void SpecialAttack(Fighter enemy)
     {
         enemy.TakeDamage(Damage);
-        Health += Damage / 2;
-        Console.WriteLine(Messages.HealAndDamageMessage, Name, Damage, Damage / 2, enemy.Name, enemy.Health);
+        Health += Damage / HealMultiplier;
+        Console.WriteLine(Messages.HealAndDamageMessage, Name, Damage, Damage / HealMultiplier, enemy.Name, enemy.Health);
     }
 }
 
 public class Mage : Fighter
 {
-    public int Mana { get; set; }
+    public int Mana { get; private set; }
+    private const int MagicDamageMultiplier = 2;
+    private const int ManaCost = 10;
 
     public Mage(string name, int health, int damage, int mana) : base(name, health, damage)
     {
@@ -175,11 +163,11 @@ public class Mage : Fighter
 
     public override void SpecialAttack(Fighter enemy)
     {
-        if (Mana >= 10)
+        if (Mana >= ManaCost)
         {
-            enemy.TakeDamage(Damage * 2);
-            Mana -= 10;
-            Console.WriteLine(Messages.MagicDamageMessage, Name, Damage * 2, Mana, enemy.Name, enemy.Health);
+            enemy.TakeDamage(Damage * MagicDamageMultiplier);
+            Mana -= ManaCost;
+            Console.WriteLine(Messages.MagicDamageMessage, Name, Damage * MagicDamageMultiplier, Mana, enemy.Name, enemy.Health);
         }
         else
         {
